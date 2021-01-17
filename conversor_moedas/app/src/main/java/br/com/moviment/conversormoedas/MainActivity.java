ee98027d9eba391ecb7455d59a5710cd713a19e2
michelmarciano <michel.marciano1984@gmail.com>
2021-01-17 12:39:19 -0300

    initial commit

---
+++ conversor_moedas/app/src/main/java/br/com/moviment/conversormoedas/MainActivity.java
@@ -0,0 +1,44 @@
+package br.com.moviment.conversormoedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_calculate){
+            //Logica
+        }
+
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
+
+
+    }
+}
